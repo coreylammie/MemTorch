af288d08690524711015a0fe840edd78a600c6c8
coreylammie <corey.lammie@jcu.edu.au>
2020-09-08 10:05:11 +1000

    Further increased codecov

---
+++ tests/test_dataloaders.py
@@ -0,0 +1,16 @@
+import pytest
+import torch
+import memtorch
+from memtorch.utils import LoadMNIST, LoadCIFAR10
+
+
+@pytest.mark.parametrize("dataloader", [LoadMNIST, LoadCIFAR10])
+def test_dataloader(dataloader):
+    train_loader, validation_loader, test_loader = dataloader(validation=True)
+    assert train_loader is not None
+    assert validation_loader is not None
+    assert test_loader is not None
+    train_loader, validation_loader, test_loader = dataloader(validation=False)
+    assert train_loader is not None
+    assert validation_loader is None
+    assert test_loader is not None
