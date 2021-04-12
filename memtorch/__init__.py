6834501dc380e3b643ba8e0980772a459b2d156d
Corey Lammie <coreylammie@gmail.com>
2021-04-12 21:42:35 +1000

    Added .isort.cgf to Avoid Sorting __init__.py in MemTorch

--- memtorch/__init__.py
+++ memtorch/__init__.py
@@ -1,10 +1,9 @@
-import torch
+from .version import __version__
 
+import torch
 from memtorch.bh import *
 from memtorch.bh.nonideality import *
 from memtorch.map import *
 from memtorch.mn import *
 from memtorch.submodules import *
 from memtorch.utils import *
-
-from .version import __version__
