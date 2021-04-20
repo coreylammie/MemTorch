2de570fa553d44690791eec76ab3e1a1fe5f31ef
Corey Lammie <corey.lammie@jcu.edu.au>
2021-04-20 11:57:26 +1000

    Updated Installation Instructions and setup.py (#42)
    * Replaced extra_include_paths with include_paths to avoid build errors and added explicit installation commands to Tutorial.ipynb
    
    * Updated Tutorial.ipynb With Updated Installation Instructions

--- setup.py
+++ setup.py
@@ -23,17 +23,22 @@ if CUDA:
 
     ext_modules = [
         CUDAExtension(
-            "cuda_quantization",
-            ["memtorch/cu/quantize/quant_cuda.cpp", "memtorch/cu/quantize/quant.cu"],
-            extra_include_paths="memtorch/cu/quantize",
+            name="cuda_quantization",
+            sources=[
+                "memtorch/cu/quantize/quant_cuda.cpp",
+                "memtorch/cu/quantize/quant.cu",
+            ],
+            include_paths="memtorch/cu/quantize",
         ),
-        CppExtension("quantization", ["memtorch/cpp/quantize/quant.cpp"]),
+        CppExtension(name="quantization", sources=["memtorch/cpp/quantize/quant.cpp"]),
     ]
     name = "memtorch"
 else:
     from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-    ext_modules = [CppExtension("quantization", ["memtorch/cpp/quantize/quant.cpp"])]
+    ext_modules = [
+        CppExtension(name="quantization", sources=["memtorch/cpp/quantize/quant.cpp"])
+    ]
     name = "memtorch-cpu"
 
 if __name__ == "__main__":
@@ -46,6 +51,7 @@ if __name__ == "__main__":
         license="GPL-3.0",
         author="Corey Lammie",
         author_email="coreylammie@jcu.edu.au",
+        setup_requires=["ninja"],
         ext_modules=ext_modules,
         cmdclass={"build_ext": BuildExtension},
         packages=find_packages(),
