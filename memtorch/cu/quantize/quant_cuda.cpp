852cd2f826a3f3fc0d215b733bc2103a0d00b9c9
coreylammie <coreylammie@users.noreply.github.com>
2021-06-11 00:24:22 +0000

    :art: Enforced Python/C++/CUDA Code Formatting with Black and Clang

--- memtorch/cu/quantize/quant_cuda.cpp
+++ memtorch/cu/quantize/quant_cuda.cpp
@@ -1,12 +1,15 @@
-#include <torch/extension.h>
 #include <ATen/cuda/CUDAContext.h>
+#include <torch/extension.h>
 #include <vector>
 
 // CUDA kernels
-void quant_cuda(at::Tensor tensor, int num_quant_levels, float min_value, float max_value);
-void quant_cuda(at::Tensor tensor, int num_quant_levels, at::Tensor min_values, at::Tensor max_values);
+void quant_cuda(at::Tensor tensor, int num_quant_levels, float min_value,
+                float max_value);
+void quant_cuda(at::Tensor tensor, int num_quant_levels, at::Tensor min_values,
+                at::Tensor max_values);
 
-void quant(at::Tensor tensor, int num_quant_levels, float min_value, float max_value) {
+void quant(at::Tensor tensor, int num_quant_levels, float min_value,
+           float max_value) {
   if (at::cuda::is_available()) {
     tensor.to(torch::Device("cuda:0"));
     quant_cuda(tensor, num_quant_levels, min_value, max_value);
@@ -15,7 +18,8 @@ void quant(at::Tensor tensor, int num_quant_levels, float min_value, float max_v
   }
 }
 
-void quant(at::Tensor tensor, int num_quant_levels, at::Tensor min_values, at::Tensor max_values) {
+void quant(at::Tensor tensor, int num_quant_levels, at::Tensor min_values,
+           at::Tensor max_values) {
   if (at::cuda::is_available()) {
     assert(tensor.numel() == min_values.numel() == max_values.numel());
     tensor.to(torch::Device("cuda:0"));
@@ -28,6 +32,7 @@ void quant(at::Tensor tensor, int num_quant_levels, at::Tensor min_values, at::T
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("quantize", (void (*)(at::Tensor, int, float, float)) &quant, "tbd");
-  m.def("quantize", (void (*)(at::Tensor, int, at::Tensor, at::Tensor)) &quant, "tbd");
+  m.def("quantize", (void (*)(at::Tensor, int, float, float)) & quant, "tbd");
+  m.def("quantize", (void (*)(at::Tensor, int, at::Tensor, at::Tensor)) & quant,
+        "tbd");
 }
