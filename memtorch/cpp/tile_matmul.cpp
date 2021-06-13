747062b5cc44826bddabec1d4276705cb79ba7c9
Corey Lammie <coreylammie@gmail.com>
2021-06-13 20:27:11 +1000

    Added Minimal Working Implementation of time_matmul in C++

--- memtorch/cpp/tile_matmul.cpp
+++ memtorch/cpp/tile_matmul.cpp
@@ -2,15 +2,51 @@
 #include <cmath>
 #include <torch/extension.h>
 
-void tile_matmul(at::Tensor mat_a_tiles, at::Tensor mat_a_tiles_map,
-                 int mat_a_shape[2], at::Tensor mat_b_tiles,
-                 at::Tensor mat_b_tiles_map, int mat_b_shape[2]) {
+using namespace torch::indexing;
+
+at::Tensor tile_matmul_row(at::Tensor mat_a_row_tiles,
+                           at::Tensor mat_a_tiles_map, at::Tensor mat_b_tiles,
+                           at::Tensor mat_b_tiles_map, int mat_b_shape[2],
+                           at::Tensor partial_sum) {
+  partial_sum = partial_sum.zero_();
+  at::Tensor mat_b_tiles_map_shape = at::_shape_as_tensor(mat_b_tiles_map);
+#pragma omp parallel for schedule(dynamic, 1) collapse(2)
+  for (int j = 0; j < mat_b_tiles_map_shape[1].item<int>(); j++) {
+    for (int i = 0; i < mat_b_tiles_map_shape[0].item<int>(); i++) {
+      at::Tensor tile_a =
+          mat_a_row_tiles[mat_a_tiles_map[i].item<int>()]; //.to(torch::kCUDA);
+      at::Tensor tile_b =
+          mat_b_tiles[mat_b_tiles_map[i][j].item<int>()]; //.to(torch::kCUDA);
+      partial_sum[j] += at::matmul(tile_a, tile_b).squeeze();
+    }
+  }
+  return partial_sum.flatten().index({Slice(0, mat_b_shape[1])});
+}
+
+at::Tensor tile_matmul(at::Tensor mat_a_tiles, at::Tensor mat_a_tiles_map,
+                       int mat_a_shape[2], at::Tensor mat_b_tiles,
+                       at::Tensor mat_b_tiles_map, int mat_b_shape[2]) {
   at::Tensor mat_b_n_dim = at::_shape_as_tensor(mat_b_tiles);
-  int tile_shape[2] = {mat_b_n_dim[-2].item<int>(),
-                       mat_b_n_dim[-1].item<int>()};
-  std::cout << tile_shape[0] << std::endl;
-  std::cout << tile_shape[1] << std::endl;
-  return;
+  at::Tensor partial_sum =
+      at::zeros({mat_b_n_dim[-2].item<int>(),
+                 mat_b_n_dim[-1].item<int>()}); // .to(torch::kCUDA);
+  if (at::_shape_as_tensor(mat_a_tiles)[-2].item<int>() > 1) {
+    at::Tensor result =
+        at::zeros({mat_a_shape[0], mat_b_shape[1]}); //.to(torch::kCUDA);
+#pragma omp parallel for
+    for (int i = 0; i < at::_shape_as_tensor(mat_a_tiles)[-2].item<int>();
+         i++) {
+      result.index_put_(
+          {i, Slice()},
+          tile_matmul_row(mat_a_tiles.index({Slice(), i, Slice()}),
+                          mat_a_tiles_map, mat_b_tiles, mat_b_tiles_map,
+                          mat_b_shape, partial_sum));
+    }
+    return result;
+  } else {
+    return tile_matmul_row(mat_a_tiles, mat_a_tiles_map, mat_b_tiles,
+                           mat_b_tiles_map, mat_b_shape, partial_sum);
+  }
 }
 
 void tile_matmul_bindings(py::module_ &m) {
@@ -24,7 +60,7 @@ void tile_matmul_bindings(py::module_ &m) {
                                       (int)std::get<1>(mat_a_shape)};
           int mat_b_shape_array[2] = {(int)std::get<0>(mat_b_shape),
                                       (int)std::get<1>(mat_b_shape)};
-          tile_matmul(mat_a_tiles, mat_a_tiles_map, mat_a_shape_array,
-                      mat_b_tiles, mat_b_tiles_map, mat_b_shape_array);
+          return tile_matmul(mat_a_tiles, mat_a_tiles_map, mat_a_shape_array,
+                             mat_b_tiles, mat_b_tiles_map, mat_b_shape_array);
         });
 }
--- temp.py
+++ temp.py
@@ -1,17 +1,33 @@
+import time
+
 import torch
+
 import memtorch
 import memtorch_bindings
 from memtorch.bh.crossbar.Tile import gen_tiles
 
 tile_shape = (16, 16)
-test_shape = (25, 25)
-a = torch.zeros(test_shape).uniform_(0, 1)
-b = torch.zeros(test_shape).uniform_(0, 1)
+
+test_shape_a = (200, 255)
+test_shape_b = (255, 255)
+
+a = torch.zeros(test_shape_a).uniform_(0, 1)
+b = torch.zeros(test_shape_b).uniform_(0, 1)
 
 tile_a_tiles, tile_a_map = gen_tiles(a, tile_shape, input=True)
 tile_b_tiles, tile_b_map = gen_tiles(b, tile_shape, input=False)
 
-print(tile_b_tiles.shape[-2:])
-memtorch_bindings.tile_matmul(
-    tile_a_tiles, tile_a_map, test_shape, tile_b_tiles, tile_a_map, test_shape
+start_time = time.time()
+python_res = memtorch.bh.crossbar.tile_matmul(
+    tile_a_tiles, tile_a_map, test_shape_a, tile_b_tiles, tile_b_map, test_shape_b
 )
+elapsed_time = time.time() - start_time
+print(python_res)
+print(elapsed_time)
+start_time = time.time()
+cpp_res = memtorch_bindings.tile_matmul(
+    tile_a_tiles, tile_a_map, test_shape_a, tile_b_tiles, tile_b_map, test_shape_b
+)
+elapsed_time = time.time() - start_time
+print(cpp_res)
+print(elapsed_time)
