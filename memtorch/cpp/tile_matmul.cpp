f549515c9eca716ab9a313a6ae299c837b14ef11
Corey Lammie <coreylammie@gmail.com>
2021-06-28 12:38:42 +1000

    Added C++ Implementation of tile_matmul With Quantization Support

--- memtorch/cpp/tile_matmul.cpp
+++ memtorch/cpp/tile_matmul.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <torch/extension.h>
 
+#include "quantize.h"
 using namespace torch::indexing;
 
 at::Tensor tile_matmul(at::Tensor mat_a_tiles, at::Tensor mat_a_tiles_map,
@@ -31,6 +32,36 @@ at::Tensor tile_matmul(at::Tensor mat_a_tiles, at::Tensor mat_a_tiles_map,
   return result;
 }
 
+at::Tensor tile_matmul(at::Tensor mat_a_tiles, at::Tensor mat_a_tiles_map,
+                       int mat_a_shape[2], at::Tensor mat_b_tiles,
+                       at::Tensor mat_b_tiles_map, int mat_b_shape[2],
+                       int ADC_resolution, float ADC_overflow_rate,
+                       int quant_method) {
+  int mat_a_rows = mat_a_tiles.sizes().end()[-2];
+  c10::IntArrayRef mat_b_tiles_shape = mat_b_tiles.sizes();
+  c10::IntArrayRef mat_b_tiles_map_shape = mat_b_tiles_map.sizes();
+  at::Tensor partial_sum =
+      at::zeros({mat_b_tiles_map_shape[1], mat_b_tiles_shape.back()});
+  at::Tensor result = at::zeros({mat_a_shape[0], mat_b_shape[1]});
+#pragma omp parallel for
+  for (int i = 0; i < mat_a_rows; i++) {
+    at::Tensor mat_a_row_tiles = mat_a_tiles.index({Slice(), i, Slice()});
+    for (int j = 0; j < mat_b_tiles_map_shape[0]; j++) {
+      partial_sum =
+          at::zeros({mat_b_tiles_map_shape[1], mat_b_tiles_shape.back()});
+      at::Tensor tile_a = mat_a_row_tiles[mat_a_tiles_map[j].item<int>()];
+      for (int k = 0; k < mat_b_tiles_map_shape[1]; k++) {
+        at::Tensor tile_b = mat_b_tiles[mat_b_tiles_map[j][k].item<int>()];
+        partial_sum[k] += at::matmul(tile_a, tile_b).squeeze();
+      }
+      partial_sum = partial_sum.flatten().index({Slice(0, mat_b_shape[1])});
+      quantize(partial_sum, ADC_resolution, ADC_overflow_rate, quant_method);
+      result.index_put_({i, Slice()}, result.index({i, Slice()}) + partial_sum);
+    }
+  }
+  return result;
+}
+
 void tile_matmul_bindings(py::module_ &m) {
   m.def("tile_matmul",
         [](at::Tensor mat_a_tiles, at::Tensor mat_a_tiles_map,
@@ -44,5 +75,20 @@ void tile_matmul_bindings(py::module_ &m) {
                                       (int)std::get<1>(mat_b_shape)};
           return tile_matmul(mat_a_tiles, mat_a_tiles_map, mat_a_shape_array,
                              mat_b_tiles, mat_b_tiles_map, mat_b_shape_array);
+        });
+  m.def("tile_matmul",
+        [](at::Tensor mat_a_tiles, at::Tensor mat_a_tiles_map,
+           std::tuple<int, float> mat_a_shape, at::Tensor mat_b_tiles,
+           at::Tensor mat_b_tiles_map, std::tuple<int, float> mat_b_shape,
+           int ADC_resolution, float ADC_overflow_rate, int quant_method) {
+          assert((std::tuple_size<int, float>(mat_a_shape) == 2) &&
+                 (std::tuple_size<int, float>(mat_b_shape) == 2));
+          int mat_a_shape_array[2] = {(int)std::get<0>(mat_a_shape),
+                                      (int)std::get<1>(mat_a_shape)};
+          int mat_b_shape_array[2] = {(int)std::get<0>(mat_b_shape),
+                                      (int)std::get<1>(mat_b_shape)};
+          return tile_matmul(mat_a_tiles, mat_a_tiles_map, mat_a_shape_array,
+                             mat_b_tiles, mat_b_tiles_map, mat_b_shape_array,
+                             ADC_resolution, ADC_overflow_rate, quant_method);
         });
 }
