b967adeea5a82dfc4623eeaa4e29dc5133786ef7
coreylammie <coreylammie@users.noreply.github.com>
2021-06-29 05:59:22 +0000

    :art: Enforced Python/C++/CUDA Code Formatting with Black and Clang

--- debug_quantization.py
+++ debug_quantization.py
@@ -17,10 +17,12 @@ print(tensor)
 # Linear
 python_output = copy.deepcopy(tensor)
 memtorch_bindings.quantize(
-    python_output, bits, overflow_rate, quant_method=0, min=0.1, max=0.9)
+    python_output, bits, overflow_rate, quant_method=0, min=0.1, max=0.9
+)
 print(python_output)
 cpp_output = memtorch_cuda_bindings.quantize(
-    tensor, bits, overflow_rate, quant_method=0, min=0.1, max=0.9)
+    tensor, bits, overflow_rate, quant_method=0, min=0.1, max=0.9
+)
 print(cpp_output)
 # Log
 # TBD ....
