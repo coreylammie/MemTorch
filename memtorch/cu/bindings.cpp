603f6593e3584e48ee810758d7bf687812559546
Corey Lammie <coreylammie@gmail.com>
2021-08-09 12:02:41 +1000

    Added GPU Bindings for tiled_inference

--- memtorch/cu/bindings.cpp
+++ memtorch/cu/bindings.cpp
@@ -3,12 +3,15 @@
 #include <torch/extension.h>
 
 #include "gen_tiles.h"
+#include "inference.h"
 #include "tile_matmul.h"
 
 void tile_matmul_bindings(py::module_ &);
 void gen_tiles_bindings_gpu(py::module_ &);
+void inference_bindings(py::module_ &);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   gen_tiles_bindings_gpu(m);
   tile_matmul_bindings(m);
+  inference_bindings(m);
 }
--- memtorch/cu/inference.cpp
+++ memtorch/cu/inference.cpp
@@ -3,31 +3,33 @@
 #include <torch/extension.h>
 
 #include "gen_tiles.h"
-#include "tile_matmul.h"
+#include "tile_matmul_kernels.cuh"
 
 at::Tensor tiled_inference(at::Tensor input, int input_shape[2],
                            int tile_shape[2], at::Tensor weight_tiles,
-                           at::Tensor weight_tiles_map, int weight_shape[2]) {
+                           at::Tensor weight_tiles_map, int weight_shape[2],
+                           int cuda_malloc_heap_size) {
   at::Tensor input_tiles;
   at::Tensor input_tiles_map;
   std::tie(input_tiles, input_tiles_map) = gen_tiles(
-      input, tile_shape, true, torch::TensorOptions().device(torch::kCPU));
+      input, tile_shape, true, torch::TensorOptions().device(torch::kCUDA, 0));
   return tile_matmul(input_tiles, input_tiles_map, input_shape, weight_tiles,
-                     weight_tiles_map, weight_shape);
+                     weight_tiles_map, weight_shape, NULL, NULL, -1,
+                     cuda_malloc_heap_size);
 }
 
 at::Tensor tiled_inference(at::Tensor input, int input_shape[2],
                            int tile_shape[2], at::Tensor weight_tiles,
                            at::Tensor weight_tiles_map, int weight_shape[2],
                            int ADC_resolution, float ADC_overflow_rate,
-                           int quant_method) {
+                           int quant_method, int cuda_malloc_heap_size) {
   at::Tensor input_tiles;
   at::Tensor input_tiles_map;
   std::tie(input_tiles, input_tiles_map) = gen_tiles(
-      input, tile_shape, true, torch::TensorOptions().device(torch::kCPU));
+      input, tile_shape, true, torch::TensorOptions().device(torch::kCUDA, 0));
   return tile_matmul(input_tiles, input_tiles_map, input_shape, weight_tiles,
                      weight_tiles_map, weight_shape, ADC_resolution,
-                     ADC_overflow_rate, quant_method);
+                     ADC_overflow_rate, quant_method, cuda_malloc_heap_size);
 }
 
 void inference_bindings(py::module_ &m) {
@@ -36,7 +38,8 @@ void inference_bindings(py::module_ &m) {
       "tiled_inference",
       [](at::Tensor input, std::tuple<int, float> input_shape,
          std::tuple<int, float> tile_shape, at::Tensor weight_tiles,
-         at::Tensor weight_tiles_map, std::tuple<int, float> weight_shape) {
+         at::Tensor weight_tiles_map, std::tuple<int, float> weight_shape,
+         int cuda_malloc_heap_size) {
         assert((std::tuple_size<int, float>(input_shape) == 2));
         assert((std::tuple_size<int, float>(tile_shape) == 2));
         assert((std::tuple_size<int, float>(weight_shape) == 3));
@@ -48,18 +51,19 @@ void inference_bindings(py::module_ &m) {
                                      (int)std::get<1>(weight_shape)};
         return tiled_inference(input, input_shape_array, tile_shape_array,
                                weight_tiles, weight_tiles_map,
-                               weight_shape_array);
+                               weight_shape_array, cuda_malloc_heap_size);
       },
       py::arg("input"), py::arg("input_shape"), py::arg("tile_shape"),
       py::arg("weight_tiles"), py::arg("weight_tiles_map"),
-      py::arg("weight_shape"));
+      py::arg("weight_shape"), py::arg("cuda_malloc_heap_size") = 50);
   // Binding with quantization support
   m.def(
       "tiled_inference",
       [](at::Tensor input, std::tuple<int, float> input_shape,
          std::tuple<int, float> tile_shape, at::Tensor weight_tiles,
          at::Tensor weight_tiles_map, std::tuple<int, float> weight_shape,
-         int ADC_resolution, float ADC_overflow_rate, int quant_method) {
+         int ADC_resolution, float ADC_overflow_rate, int quant_method,
+         int cuda_malloc_heap_size) {
         assert((std::tuple_size<int, float>(input_shape) == 2));
         assert((std::tuple_size<int, float>(tile_shape) == 2));
         assert((std::tuple_size<int, float>(weight_shape) == 3));
@@ -69,13 +73,14 @@ void inference_bindings(py::module_ &m) {
                                    (int)std::get<1>(tile_shape)};
         int weight_shape_array[2] = {(int)std::get<0>(weight_shape),
                                      (int)std::get<1>(weight_shape)};
-        return tiled_inference(input, input_shape_array, tile_shape_array,
-                               weight_tiles, weight_tiles_map,
-                               weight_shape_array, ADC_resolution,
-                               ADC_overflow_rate, quant_method);
+        return tiled_inference(
+            input, input_shape_array, tile_shape_array, weight_tiles,
+            weight_tiles_map, weight_shape_array, ADC_resolution,
+            ADC_overflow_rate, quant_method, cuda_malloc_heap_size);
       },
       py::arg("input"), py::arg("input_shape"), py::arg("tile_shape"),
       py::arg("weight_tiles"), py::arg("weight_tiles_map"),
       py::arg("weight_shape"), py::arg("ADC_resolution"),
-      py::arg("ADC_overflow_rate"), py::arg("quant_method"));
+      py::arg("ADC_overflow_rate"), py::arg("quant_method"),
+      py::arg("cuda_malloc_heap_size") = 50);
 }
