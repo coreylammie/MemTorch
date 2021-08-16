b5c747f1e98dc0c863e52fd8fc300f98eb99e904
Corey Lammie <coreylammie@gmail.com>
2021-08-16 16:30:30 +1000

    Removed SuiteSparse submodule and replaced with Eigen

--- memtorch/cpp/bindings.cpp
+++ memtorch/cpp/bindings.cpp
@@ -5,16 +5,19 @@
 #include "gen_tiles.h"
 #include "inference.h"
 #include "quantize.h"
+#include "solve_sparse_linear.h"
 #include "tile_matmul.h"
 
 void quantize_bindings(py::module_ &);
 void gen_tiles_bindings(py::module_ &);
 void tile_matmul_bindings(py::module_ &);
 void inference_bindings(py::module_ &);
+void solve_sparse_linear_bindings(py::module_ &);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   quantize_bindings(m);
   gen_tiles_bindings(m);
   tile_matmul_bindings(m);
   inference_bindings(m);
+  solve_sparse_linear_bindings(m);
 }
