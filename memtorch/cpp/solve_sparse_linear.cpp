47255434ce6b056b1794d20233922438dab48be3
Corey Lammie <coreylammie@gmail.com>
2021-08-17 11:56:52 +1000

    Developed Functional C++ Sparse Linear Matrix Solver

--- memtorch/cpp/solve_sparse_linear.cpp
+++ memtorch/cpp/solve_sparse_linear.cpp
@@ -8,22 +8,44 @@
 
 #include <Eigen/SparseCholesky>
 
-void solve_sparse_linear() {
+Eigen::VectorXf
+solve_sparse_linear(Eigen::VectorXf A_indices_x, Eigen::VectorXf A_indices_y,
+                    Eigen::VectorXf A_values, int A_nonzero_elements,
+                    std::tuple<int, int> A_shape, Eigen::VectorXf B) {
   std::vector<Eigen::Triplet<float>> triplet_list;
-  triplet_list.reserve(1);
-  triplet_list.push_back(Eigen::Triplet<float>(5, 5, 1.5f));
-
-  Eigen::SparseMatrix<float> a(10, 10);
-  a.setFromTriplets(triplet_list.begin(), triplet_list.end());
-  Eigen::VectorXf b(10);
-  b[0] = 2.0f;
-
+  triplet_list.reserve(A_nonzero_elements);
+  for (int i = 0; i < A_nonzero_elements; i++) {
+    triplet_list.push_back(
+        Eigen::Triplet<float>(A_indices_x[i], A_indices_y[i], A_values[i]));
+  }
+  Eigen::SparseMatrix<float> A(std::get<0>(A_shape), std::get<1>(A_shape));
+  A.setFromTriplets(triplet_list.begin(), triplet_list.end());
   Eigen::SimplicialLDLT<Eigen::SparseMatrix<float>> solver;
-  solver.compute(a);
-  Eigen::VectorXf x = solver.solve(b);
-  std::cout << x << std::endl;
+  solver.compute(A);
+  return solver.solve(B);
 }
 
 void solve_sparse_linear_bindings(py::module_ &m) {
-  m.def("solve_sparse_linear", &solve_sparse_linear);
+  m.def("solve_sparse_linear", [](at::Tensor A_indices_x,
+                                  at::Tensor A_indices_y, at::Tensor A_values,
+                                  std::tuple<int, int> A_shape, at::Tensor B) {
+    float *A_indices_x_accessor = A_indices_x.data_ptr<float>();
+    float *A_indices_y_accessor = A_indices_y.data_ptr<float>();
+    float *A_values_accessor = A_values.data_ptr<float>();
+    float *B_accessor = B.data_ptr<float>();
+    int A_nonzero_elements = A_values.sizes()[0];
+    Eigen::Map<Eigen::VectorXf> A_indices_x_vector(&A_indices_x_accessor[0],
+                                                   A_nonzero_elements);
+    Eigen::Map<Eigen::VectorXf> A_indices_y_vector(&A_indices_y_accessor[0],
+                                                   A_nonzero_elements);
+    Eigen::Map<Eigen::VectorXf> A_values_vector(&A_values_accessor[0],
+                                                A_nonzero_elements);
+    Eigen::Map<Eigen::VectorXf> B_vector(&B_accessor[0], B.sizes()[0]);
+    Eigen::VectorXf X = solve_sparse_linear(
+        A_indices_x_vector, A_indices_y_vector, A_values_vector,
+        A_nonzero_elements, A_shape, B_vector);
+    at::Tensor X_tensor = at::zeros({X.size()});
+    memcpy(X_tensor.data_ptr<float>(), X.data(), sizeof(float) * X.size());
+    return X_tensor;
+  });
 }
