de6b2d879cf9823f2caa8132175e7ce4c122e176
Corey Lammie <coreylammie@gmail.com>
2021-08-19 10:12:52 +1000

    Validated binding indices

--- memtorch/cpp/interconnect_line_source_resistance.cpp
+++ memtorch/cpp/interconnect_line_source_resistance.cpp
@@ -7,6 +7,9 @@
 
 #include <Eigen/SparseLU>
 
+#include <fstream>
+#include <iostream>
+
 typedef Eigen::Triplet<float> sparse_element;
 
 at::Tensor gen_ABCD_E(at::Tensor conductance_matrix, at::Tensor V_WL,
@@ -20,13 +23,14 @@ at::Tensor gen_ABCD_E(at::Tensor conductance_matrix, at::Tensor V_WL,
   float *V_BL_accessor = V_BL.data_ptr<float>();
   int non_zero_elements = 8 * m * n - 2 * m - 2 * n;
   std::vector<Eigen::Triplet<float>> ABCD_matrix;
-  ABCD_matrix.reserve(non_zero_elements);
+  // ABCD_matrix.reserve(non_zero_elements);
   Eigen::VectorXd mn_range = Eigen::VectorXd::LinSpaced(0, m * n - 1, m * n);
   Eigen::VectorXd m_range = Eigen::VectorXd::LinSpaced(0, m - 1, m);
   Eigen::VectorXd n_range = Eigen::VectorXd::LinSpaced(0, n - 1, n);
-// A matrix
+  // A and B matrices
 #pragma omp parallel for
   for (int i = 0; i < m; i++) {
+    // A matrix
     ABCD_matrix.push_back(sparse_element(i * n, i * n,
                                          conductance_matrix_accessor(i, 0) +
                                              1.0f / R_source + 1.0f / R_line));
@@ -35,8 +39,15 @@ at::Tensor gen_ABCD_E(at::Tensor conductance_matrix, at::Tensor V_WL,
     ABCD_matrix.push_back(
         sparse_element(i * n + (n - 1), i * n + (n - 1),
                        conductance_matrix_accessor(i, n - 1) + 1.0 / R_line));
+    // B matrix
+    ABCD_matrix.push_back(sparse_element(i * n, i * n + (m * n),
+                                         -conductance_matrix_accessor(i, 0)));
+    ABCD_matrix.push_back(
+        sparse_element(i * n + (n - 1), i * n + (n - 1) + (m * n),
+                       -conductance_matrix_accessor(i, n - 1)));
 #pragma omp for nowait
     for (int j = 1; j < n - 1; j++) {
+      // A matrix
       ABCD_matrix.push_back(
           sparse_element(i * n + j, i * n + j,
                          conductance_matrix_accessor(i, j) + 2.0f / R_line));
@@ -44,24 +55,15 @@ at::Tensor gen_ABCD_E(at::Tensor conductance_matrix, at::Tensor V_WL,
           sparse_element(i * n + j + 1, i * n + j, -1.0f / R_line));
       ABCD_matrix.push_back(
           sparse_element(i * n + j, i * n + j + 1, -1.0f / R_line));
+      // B matrix
+      ABCD_matrix.push_back(sparse_element(i * n + j, i * n + j + (m * n),
+                                           -conductance_matrix_accessor(i, j)));
     }
   }
-  // B and C matrices
-  int j;
-  int k;
-#pragma omp parallel for
-  for (int i = 0; i < m * n; i++) {
-    j = i % m;
-    k = i / m;
-    ABCD_matrix.push_back(sparse_element(
-        i, i + (m * n), -conductance_matrix_accessor(j, k))); // B matrix
-    ABCD_matrix.push_back(
-        sparse_element(i + (m * n), n * (j - 1) + k,
-                       conductance_matrix_accessor(j, k))); // C matrix
-  }
-// D matrix
+  // C and D matrices
 #pragma omp parallel for
   for (int j = 0; j < n; j++) {
+    // D matrix
     ABCD_matrix.push_back(
         sparse_element(m * n + (j * m), m * n + j,
                        -1.0f / R_line - conductance_matrix_accessor(0, j)));
@@ -73,8 +75,15 @@ at::Tensor gen_ABCD_E(at::Tensor conductance_matrix, at::Tensor V_WL,
         m * n + (j * m) + m - 1, m * n + (n * (m - 1)) + j,
         -1.0f / R_source - conductance_matrix_accessor(m - 1, j) -
             1.0f / R_line));
+    // C matrix
+    ABCD_matrix.push_back(
+        sparse_element(j * m + (m * n), j, conductance_matrix_accessor(0, j)));
+    ABCD_matrix.push_back(
+        sparse_element(j * m + (m - 1) + (m * n), n * (m - 1) + j,
+                       conductance_matrix_accessor(m - 1, j)));
 #pragma omp for nowait
     for (int i = 1; i < m - 1; i++) {
+      // D matrix
       ABCD_matrix.push_back(sparse_element(
           m * n + (j * m) + i, m * n + (n * (i - 1)) + j, 1.0f / R_line));
       ABCD_matrix.push_back(sparse_element(
@@ -82,27 +91,50 @@ at::Tensor gen_ABCD_E(at::Tensor conductance_matrix, at::Tensor V_WL,
       ABCD_matrix.push_back(
           sparse_element(m * n + (j * m) + i, m * n + (n * i) + j,
                          -conductance_matrix_accessor(i, j) - 2.0f / R_line));
+      // C matrix
+      ABCD_matrix.push_back(sparse_element(j * m + i + (m * n), n * i + j,
+                                           conductance_matrix_accessor(i, j)));
     }
   }
+  // temp- debugging
+  std::ofstream myfile;
+  myfile.open("binding.csv");
+  for (auto &el : ABCD_matrix) {
+    myfile << el.row() << "," << el.col() << "," << el.value() << "\n";
+  }
+  myfile.close();
+  std::cout << "0" << std::endl;
+  // Eigen::SparseMatrix<float> ABCD(2 * m * n, 2 * m * n);
+  // std::cout << "1" << std::endl;
+  // // ABCD.setFromTriplets(ABCD_matrix.begin(), ABCD_matrix.end());
+  // ABCD.setFromTriplets(ABCD_matrix.begin(), ABCD_matrix.end());
+  // //  [](const float &a, const float &b) { return a + b; });
+  // std::cout << "2" << std::endl;
+  // Eigen::MatrixXf ABCD_dense = Eigen::MatrixXf(ABCD);
+  // std::cout << ABCD_dense << std::endl;
+
   // E matrix
-  Eigen::VectorXf E_matrix = Eigen::VectorXf::Zero(2 * m * n);
-#pragma omp parallel for
-  for (int i = 0; i < m; i++) {
-    E_matrix(i * n) = V_WL_accessor[i] / R_source;
-  }
-#pragma omp parallel for
-  for (int i = 0; i < n; i++) {
-    E_matrix(m * n + (i + 1) * m - 1) = -V_BL_accessor[i] / R_source;
-  }
-  // Solve (ABCD)V = E
-  Eigen::SparseMatrix<float> ABCD(2 * m * n, 2 * m * n);
-  ABCD.setFromTriplets(ABCD_matrix.begin(), ABCD_matrix.end());
-  Eigen::SparseLU<Eigen::SparseMatrix<float>> solver;
-  solver.compute(ABCD);
-  Eigen::VectorXf V = solver.solve(E_matrix);
-  at::Tensor V_tensor = at::zeros({V.size()});
-  memcpy(V_tensor.data_ptr<float>(), V.data(), sizeof(float) * V.size());
-  return V_tensor;
+  //   Eigen::VectorXf E_matrix = Eigen::VectorXf::Zero(2 * m * n);
+  // #pragma omp parallel for
+  //   for (int i = 0; i < m; i++) {
+  //     E_matrix(i * n) = V_WL_accessor[i] / R_source;
+  //   }
+  // #pragma omp parallel for
+  //   for (int i = 0; i < n; i++) {
+  //     E_matrix(m * n + (i + 1) * m - 1) = -V_BL_accessor[i] / R_source;
+  //   }
+  //   // Solve (ABCD)V = E
+  //   Eigen::SparseMatrix<float> ABCD(2 * m * n, 2 * m * n);
+  //   ABCD.setFromTriplets(ABCD_matrix.begin(), ABCD_matrix.end());
+  //   Eigen::SparseLU<Eigen::SparseMatrix<float>> solver;
+  //   solver.compute(ABCD);
+  //   Eigen::VectorXf V = solver.solve(E_matrix);
+  //   at::Tensor V_tensor = at::zeros({V.size()});
+  //   memcpy(V_tensor.data_ptr<float>(), V.data(), sizeof(float) *
+  //   V.size());
+  // return V_tensor;
+  at::Tensor tmp;
+  return tmp;
 }
 
 void interconnect_line_source_resistance_bindings(py::module_ &m) {
