92a70fd73404ac275d93e372dbf07f8fd644a4ef
Corey Lammie <coreylammie@gmail.com>
2021-08-23 12:49:54 +1000

    Developed Base Gen_AB CUDA Kernel

--- memtorch/cu/bindings.cpp
+++ memtorch/cu/bindings.cpp
@@ -2,16 +2,14 @@
 #include <cmath>
 #include <torch/extension.h>
 
+#include "gen_ABCD_kernels.cuh"
 #include "gen_tiles.h"
 #include "inference.h"
 #include "tile_matmul.h"
-
-void tile_matmul_bindings(py::module_ &);
-void gen_tiles_bindings_gpu(py::module_ &);
-void inference_bindings(py::module_ &);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   gen_tiles_bindings_gpu(m);
   tile_matmul_bindings(m);
   inference_bindings(m);
+  gen_ABCD_bindings(m);
 }
