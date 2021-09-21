598ae5712b11142fb04f19528134f03c71ba77cb
Corey Lammie <coreylammie@gmail.com>
2021-09-21 10:13:01 +1000

    Modularized solve_passive.cpp

--- memtorch/cpp/solve_passive.cpp
+++ memtorch/cpp/solve_passive.cpp
@@ -8,22 +8,14 @@
 #include <Eigen/SparseLU>
 
 typedef Eigen::Triplet<float> sparse_element;
+typedef std::vector<Eigen::Triplet<float>> triplet_vector;
 
-at::Tensor solve_passive(at::Tensor conductance_matrix, at::Tensor V_WL,
-                         at::Tensor V_BL, float R_source, float R_line,
-                         bool det_readout_currents) {
-  int m = conductance_matrix.sizes()[0];
-  int n = conductance_matrix.sizes()[1];
-  Eigen::Map<Eigen::MatrixXf, Eigen::RowMajor, Eigen::Stride<1, Eigen::Dynamic>>
-      conductance_matrix_accessor(conductance_matrix.data_ptr<float>(), m, n,
-                                  Eigen::Stride<1, Eigen::Dynamic>(1, n));
-  float *V_WL_accessor = V_WL.data_ptr<float>();
-  float *V_BL_accessor = V_BL.data_ptr<float>();
-  int non_zero_elements = 8 * m * n - 2 * m - 2 * n;
-  std::vector<Eigen::Triplet<float>> ABCD_matrix;
-  ABCD_matrix.reserve(non_zero_elements);
-  Eigen::VectorXf E_matrix = Eigen::VectorXf::Zero(2 * m * n);
-  // A, B, and E (partial) matrices
+triplet_vector gen_ABCDE(Eigen::MatrixXf conductance_matrix_accessor, int m,
+                         int n, float *V_WL_accessor, float *V_BL_accessor,
+                         float R_source, float R_line,
+                         triplet_vector ABCD_matrix,
+                         float *E_matrix_accessor = NULL) {
+// A, B, and E (partial) matrices
 #pragma omp parallel for
   for (int i = 0; i < m; i++) {
     // A matrix
@@ -42,7 +34,9 @@ at::Tensor solve_passive(at::Tensor conductance_matrix, at::Tensor V_WL,
         sparse_element(i * n + (n - 1), i * n + (n - 1) + (m * n),
                        -conductance_matrix_accessor(i, n - 1)));
     // E matrix
-    E_matrix(i * n) = V_WL_accessor[i] / R_source;
+    if (E_matrix_accessor != NULL) {
+      E_matrix_accessor[i * n] = V_WL_accessor[i] / R_source;
+    }
 #pragma omp for nowait
     for (int j = 1; j < n - 1; j++) {
       // A matrix
@@ -80,7 +74,9 @@ at::Tensor solve_passive(at::Tensor conductance_matrix, at::Tensor V_WL,
         sparse_element(j * m + (m - 1) + (m * n), n * (m - 1) + j,
                        conductance_matrix_accessor(m - 1, j)));
     // E matrix
-    E_matrix(m * n + (j + 1) * m - 1) = -V_BL_accessor[j] / R_source;
+    if (E_matrix_accessor != NULL) {
+      E_matrix_accessor[m * n + (j + 1) * m - 1] = -V_BL_accessor[j] / R_source;
+    }
 #pragma omp for nowait
     for (int i = 1; i < m - 1; i++) {
       // D matrix
@@ -96,6 +92,31 @@ at::Tensor solve_passive(at::Tensor conductance_matrix, at::Tensor V_WL,
                                            conductance_matrix_accessor(i, j)));
     }
   }
+  return ABCD_matrix;
+}
+
+at::Tensor solve_passive(at::Tensor conductance_matrix, at::Tensor V_WL,
+                         at::Tensor V_BL, float R_source, float R_line,
+                         bool det_readout_currents) {
+  int m = conductance_matrix.sizes()[0];
+  int n = conductance_matrix.sizes()[1];
+  Eigen::Map<Eigen::MatrixXf, Eigen::RowMajor, Eigen::Stride<1, Eigen::Dynamic>>
+      conductance_matrix_accessor(conductance_matrix.data_ptr<float>(), m, n,
+                                  Eigen::Stride<1, Eigen::Dynamic>(1, n));
+  float *V_WL_accessor = V_WL.data_ptr<float>();
+  float *V_BL_accessor = V_BL.data_ptr<float>();
+  int non_zero_elements = 8 * m * n - 2 * m - 2 * n;
+  triplet_vector ABCD_matrix;
+  ABCD_matrix.reserve(non_zero_elements);
+  float *E_matrix_accessor = (float *)malloc(sizeof(float) * (2 * m * n));
+#pragma omp parallel for
+  for (int i = 0; i < (2 * m * n); i++) {
+    E_matrix_accessor[i] = 0;
+  }
+  ABCD_matrix =
+      gen_ABCDE(conductance_matrix_accessor, m, n, V_WL_accessor, V_BL_accessor,
+                R_source, R_line, ABCD_matrix, E_matrix_accessor);
+  Eigen::Map<Eigen::VectorXf> E_matrix(E_matrix_accessor, (2 * m * n));
   // Solve (ABCD)V = E
   Eigen::SparseMatrix<float> ABCD(2 * m * n, 2 * m * n);
   ABCD.setFromTriplets(ABCD_matrix.begin(), ABCD_matrix.end());
@@ -132,77 +153,10 @@ at::Tensor solve_passive(at::Tensor conductance_matrix, at::Tensor V_WL,
       V_BL_accessor(V_BL.data_ptr<float>(), n_input_batches, n,
                     Eigen::Stride<1, Eigen::Dynamic>(1, n));
   int non_zero_elements = 8 * m * n - 2 * m - 2 * n;
-  std::vector<Eigen::Triplet<float>> ABCD_matrix;
+  triplet_vector ABCD_matrix;
   ABCD_matrix.reserve(non_zero_elements);
-  // A and B matrices
-#pragma omp parallel for
-  for (int i = 0; i < m; i++) {
-    // A matrix
-    ABCD_matrix.push_back(sparse_element(i * n, i * n,
-                                         conductance_matrix_accessor(i, 0) +
-                                             1.0f / R_source + 1.0f / R_line));
-    ABCD_matrix.push_back(sparse_element(i * n + 1, i * n, -1.0f / R_line));
-    ABCD_matrix.push_back(sparse_element(i * n, i * n + 1, -1.0f / R_line));
-    ABCD_matrix.push_back(
-        sparse_element(i * n + (n - 1), i * n + (n - 1),
-                       conductance_matrix_accessor(i, n - 1) + 1.0 / R_line));
-    // B matrix
-    ABCD_matrix.push_back(sparse_element(i * n, i * n + (m * n),
-                                         -conductance_matrix_accessor(i, 0)));
-    ABCD_matrix.push_back(
-        sparse_element(i * n + (n - 1), i * n + (n - 1) + (m * n),
-                       -conductance_matrix_accessor(i, n - 1)));
-#pragma omp for nowait
-    for (int j = 1; j < n - 1; j++) {
-      // A matrix
-      ABCD_matrix.push_back(
-          sparse_element(i * n + j, i * n + j,
-                         conductance_matrix_accessor(i, j) + 2.0f / R_line));
-      ABCD_matrix.push_back(
-          sparse_element(i * n + j + 1, i * n + j, -1.0f / R_line));
-      ABCD_matrix.push_back(
-          sparse_element(i * n + j, i * n + j + 1, -1.0f / R_line));
-      // B matrix
-      ABCD_matrix.push_back(sparse_element(i * n + j, i * n + j + (m * n),
-                                           -conductance_matrix_accessor(i, j)));
-    }
-  }
-  // C and D matrices
-#pragma omp parallel for
-  for (int j = 0; j < n; j++) {
-    // D matrix
-    ABCD_matrix.push_back(
-        sparse_element(m * n + (j * m), m * n + j,
-                       -1.0f / R_line - conductance_matrix_accessor(0, j)));
-    ABCD_matrix.push_back(
-        sparse_element(m * n + (j * m), m * n + j + n, 1.0f / R_line));
-    ABCD_matrix.push_back(sparse_element(
-        m * n + (j * m) + m - 1, m * n + (n * (m - 2)) + j, 1 / R_line));
-    ABCD_matrix.push_back(sparse_element(
-        m * n + (j * m) + m - 1, m * n + (n * (m - 1)) + j,
-        -1.0f / R_source - conductance_matrix_accessor(m - 1, j) -
-            1.0f / R_line));
-    // C matrix
-    ABCD_matrix.push_back(
-        sparse_element(j * m + (m * n), j, conductance_matrix_accessor(0, j)));
-    ABCD_matrix.push_back(
-        sparse_element(j * m + (m - 1) + (m * n), n * (m - 1) + j,
-                       conductance_matrix_accessor(m - 1, j)));
-#pragma omp for nowait
-    for (int i = 1; i < m - 1; i++) {
-      // D matrix
-      ABCD_matrix.push_back(sparse_element(
-          m * n + (j * m) + i, m * n + (n * (i - 1)) + j, 1.0f / R_line));
-      ABCD_matrix.push_back(sparse_element(
-          m * n + (j * m) + i, m * n + (n * (i + 1)) + j, 1.0f / R_line));
-      ABCD_matrix.push_back(
-          sparse_element(m * n + (j * m) + i, m * n + (n * i) + j,
-                         -conductance_matrix_accessor(i, j) - 2.0f / R_line));
-      // C matrix
-      ABCD_matrix.push_back(sparse_element(j * m + i + (m * n), n * i + j,
-                                           conductance_matrix_accessor(i, j)));
-    }
-  }
+  ABCD_matrix = gen_ABCDE(conductance_matrix_accessor, m, n, NULL, NULL,
+                          R_source, R_line, ABCD_matrix, NULL);
   // Solve (ABCD)V = E
   Eigen::SparseMatrix<float> ABCD(2 * m * n, 2 * m * n);
   ABCD.setFromTriplets(ABCD_matrix.begin(), ABCD_matrix.end());
