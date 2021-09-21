690d0d6288674ee1a48aae8891090c46e602c038
Corey Lammie <coreylammie@gmail.com>
2021-09-21 15:47:09 +1000

    Updated README and Added Source and Line Resisitance Unit Tests

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 import pytest
 import torch
+from numpy.lib import source
 
 import memtorch
 from memtorch.bh.crossbar.Program import naive_program
@@ -13,8 +14,17 @@ from memtorch.mn.Module import patch_model
 
 @pytest.mark.parametrize("tile_shape", [None, (128, 128), (10, 20)])
 @pytest.mark.parametrize("quant_method", memtorch.bh.Quantize.quant_methods + [None])
+@pytest.mark.parametrize("source_resistance", [None, 5])
+@pytest.mark.parametrize("line_resistance", [None, 5])
 @pytest.mark.parametrize("use_bindings", [True, False])
-def test_networks(debug_networks, tile_shape, quant_method, use_bindings):
+def test_networks(
+    debug_networks,
+    tile_shape,
+    quant_method,
+    source_resistance,
+    line_resistance,
+    use_bindings,
+):
     networks = debug_networks
     if quant_method is not None:
         ADC_resolution = 8
@@ -35,6 +45,8 @@ def test_networks(debug_networks, tile_shape, quant_method, use_bindings):
             max_input_voltage=1.0,
             ADC_resolution=ADC_resolution,
             quant_method=quant_method,
+            source_resistance=source_resistance,
+            line_resistance=line_resistance,
             use_bindings=use_bindings,
         )
         patched_network.tune_()
