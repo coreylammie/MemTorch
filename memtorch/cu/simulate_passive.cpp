713e14142c779cfec6d0c84c88546a669fa985e6
Corey Lammie <coreylammie@gmail.com>
2022-02-25 12:35:33 +1000

    Added force_adjustment_voltage and failure_iteration_threshold arguments to simulate_passive bindings

--- memtorch/cu/simulate_passive.cpp
+++ memtorch/cu/simulate_passive.cpp
@@ -10,12 +10,11 @@
 
 #include "simulate_passive_kernels.cuh"
 
-// Default values
-std::vector<float> r_p{2699.2336, -672.930205};
-std::vector<float> r_n{649.413746, -1474.32358};
+// Default values for r_p and r_n parameters of the Data_Driven2021 model.
+std::vector<float> r_p{2699.2336, -672.930205};  // r_p voltage-dependent resistive boundary function coefficients.
+std::vector<float> r_n{649.413746, -1474.32358}; // r_n voltage-dependent resistive boundary function coefficients.
 
 void simulate_passive_bindings(py::module_ &m) {
-
   // Data_Driven2021 model
   m.def(
       "simulate_passive",
@@ -26,6 +25,8 @@ void simulate_passive_bindings(py::module_ &m) {
           float force_adjustment_rel_tol,
           float force_adjustment_pos_voltage_threshold,
           float force_adjustment_neg_voltage_threshold,
+          float force_adjustment_voltage,
+          int failure_iteration_threshold,
           float time_series_resolution, float r_off, float r_on, float A_p,
           float A_n, float t_p, float t_n, float k_p, float k_n,
           std::vector<float> r_p, std::vector<float> r_n, float a_p, float a_n,
@@ -35,7 +36,9 @@ void simulate_passive_bindings(py::module_ &m) {
             pulse_duration, refactory_period, pos_voltage_level,
             neg_voltage_level, timeout, force_adjustment,
             force_adjustment_rel_tol, force_adjustment_pos_voltage_threshold,
-            force_adjustment_neg_voltage_threshold, time_series_resolution,
+            force_adjustment_neg_voltage_threshold,
+            force_adjustment_voltage, failure_iteration_threshold,
+            time_series_resolution,
             r_off, r_on, A_p, A_n, t_p, t_n, k_p, k_n, r_p, r_n, a_p, a_n, b_p,
             b_n, sim_neighbors);
       },
@@ -47,6 +50,8 @@ void simulate_passive_bindings(py::module_ &m) {
       py::arg("force_adjustment_rel_tol") = 1e-1,
       py::arg("force_adjustment_pos_voltage_threshold") = 0,
       py::arg("force_adjustment_neg_voltage_threshold") = 0,
+      py::arg("force_adjustment_voltage") = 0.2,
+      py::arg("failure_iteration_threshold") = 1000,
       py::arg("time_series_resolution") = 1e-10, py::arg("r_off") = 10000,
       py::arg("r_on") = 1000, py::arg("A_p") = 600.10075,
       py::arg("A_n") = -34.5988399, py::arg("t_p") = -0.0212028,
@@ -55,8 +60,7 @@ void simulate_passive_bindings(py::module_ &m) {
       py::arg("a_p") = 0.32046175, py::arg("a_n") = 0.32046175,
       py::arg("b_p") = 2.71689828, py::arg("b_n") = 2.71689828,
       py::arg("simulate_neighbours") =
-          true); // Maybe change order of simulate_neighbours to before
-                 // memristor args
+          true);
 
   // Linear Ion Drift
   m.def(
@@ -68,6 +72,8 @@ void simulate_passive_bindings(py::module_ &m) {
           float force_adjustment_rel_tol,
           float force_adjustment_pos_voltage_threshold,
           float force_adjustment_neg_voltage_threshold,
+          float force_adjustment_voltage,
+          int failure_iteration_threshold,
           float time_series_resolution, float r_off, float r_on, float u_v,
           float d, float pos_write_threshold, float neg_write_threshold,
           float p, bool sim_neighbors) {
@@ -76,7 +82,9 @@ void simulate_passive_bindings(py::module_ &m) {
             pulse_duration, refactory_period, pos_voltage_level,
             neg_voltage_level, timeout, force_adjustment,
             force_adjustment_rel_tol, force_adjustment_pos_voltage_threshold,
-            force_adjustment_neg_voltage_threshold, time_series_resolution,
+            force_adjustment_neg_voltage_threshold,
+            force_adjustment_voltage, failure_iteration_threshold,
+            time_series_resolution,
             r_off, r_on, u_v, d, pos_write_threshold, neg_write_threshold, p,
             sim_neighbors);
       },
@@ -88,6 +96,8 @@ void simulate_passive_bindings(py::module_ &m) {
       py::arg("force_adjustment_rel_tol") = 1e-1,
       py::arg("force_adjustment_pos_voltage_threshold") = 0,
       py::arg("force_adjustment_neg_voltage_threshold") = 0,
+      py::arg("force_adjustment_voltage") = 0.2,
+      py::arg("failure_iteration_threshold") = 1000,
       py::arg("time_series_resolution") = 1e-4, py::arg("r_off") = 10000,
       py::arg("r_on") = 1000, py::arg("u_v") = 1e-14, py::arg("d") = 10e-9,
       py::arg("pos_write_threshold") = 0.55,
@@ -104,6 +114,8 @@ void simulate_passive_bindings(py::module_ &m) {
           float force_adjustment_rel_tol,
           float force_adjustment_pos_voltage_threshold,
           float force_adjustment_neg_voltage_threshold,
+          float force_adjustment_voltage,
+          int failure_iteration_threshold,
           float time_series_resolution, float r_off, float r_on, float d,
           float k_on, float k_off, float alpha_on, float alpha_off, float v_on,
           float v_off, float x_on, float x_off, bool sim_neighbors) {
@@ -112,7 +124,9 @@ void simulate_passive_bindings(py::module_ &m) {
             pulse_duration, refactory_period, pos_voltage_level,
             neg_voltage_level, timeout, force_adjustment,
             force_adjustment_rel_tol, force_adjustment_pos_voltage_threshold,
-            force_adjustment_neg_voltage_threshold, time_series_resolution,
+            force_adjustment_neg_voltage_threshold,
+            force_adjustment_voltage, failure_iteration_threshold,
+            time_series_resolution,
             r_off, r_on, d, k_on, k_off, alpha_on, alpha_off, v_on, v_off, x_on,
             x_off, sim_neighbors);
       },
@@ -124,6 +138,8 @@ void simulate_passive_bindings(py::module_ &m) {
       py::arg("force_adjustment_rel_tol") = 1e-1,
       py::arg("force_adjustment_pos_voltage_threshold") = 0,
       py::arg("force_adjustment_neg_voltage_threshold") = 0,
+      py::arg("force_adjustment_voltage") = 0.2,
+      py::arg("failure_iteration_threshold") = 1000,
       py::arg("time_series_resolution") = 1e-10, py::arg("r_off") = 10000,
       py::arg("r_on") = 1000, py::arg("d") = 3e-9, py::arg("k_on") = -10,
       py::arg("k_off") = 5e-4, py::arg("alpha_on") = 3,
@@ -141,6 +157,8 @@ void simulate_passive_bindings(py::module_ &m) {
           float force_adjustment_rel_tol,
           float force_adjustment_pos_voltage_threshold,
           float force_adjustment_neg_voltage_threshold,
+          float force_adjustment_voltage,
+          int failure_iteration_threshold,
           float time_series_resolution, float r_off, float r_on, float gap_init,
           float g_0, float V_0, float I_0, float read_voltage, float T_init,
           float R_th, float gamma_init, float beta, float t_ox, float F_min,
@@ -151,7 +169,9 @@ void simulate_passive_bindings(py::module_ &m) {
             pulse_duration, refactory_period, pos_voltage_level,
             neg_voltage_level, timeout, force_adjustment,
             force_adjustment_rel_tol, force_adjustment_pos_voltage_threshold,
-            force_adjustment_neg_voltage_threshold, time_series_resolution,
+            force_adjustment_neg_voltage_threshold,
+            force_adjustment_voltage, failure_iteration_threshold,
+            time_series_resolution,
             r_off, r_on, gap_init, g_0, V_0, I_0, read_voltage, T_init, R_th,
             gamma_init, beta, t_ox, F_min, vel_0, E_a, a_0, delta_g_init,
             model_switch, T_crit, T_smth, sim_neighbors);
@@ -164,6 +184,8 @@ void simulate_passive_bindings(py::module_ &m) {
       py::arg("force_adjustment_rel_tol") = 1e-1,
       py::arg("force_adjustment_pos_voltage_threshold") = 0,
       py::arg("force_adjustment_neg_voltage_threshold") = 0,
+      py::arg("force_adjustment_voltage") = 0.2,
+      py::arg("failure_iteration_threshold") = 1000,
       py::arg("time_series_resolution") = 1e-10, py::arg("r_off") = 10000,
       py::arg("r_on") = 1000, py::arg("gap_init") = 2e-10,
       py::arg("g_0") = 0.25e-9, py::arg("V_0") = 0.25, py::arg("I_0") = 1000e-6,
